222480b2fc630679a51fb0d9c589eda5c410333d
eshom <48328872+eshom@users.noreply.github.com>
2025-04-26 10:20:33 +0300

    lzio: all header macros translated

--- src/lzio.zig
+++ src/lzio.zig
@@ -17,18 +17,16 @@ pub const ZIO = extern struct {
 };
 
 pub const Mbuffer = extern struct {
-    buffer: [*:0]const u8,
+    buffer: ?[*:0]const u8,
     n: usize,
     buffsize: usize,
 };
 
-pub fn zgetc(z: anytype) u8 {
-    const n = z.n;
-    z.n -= 1;
-    if (n > 0) {
-        const p = z.p;
-        z.p += 1;
-        return p[0];
+pub inline fn zgetc(z: *ZIO) u8 {
+    defer z.n -= 1;
+    if (z.n > 0) {
+        defer z.p += 1;
+        return z.p[0];
     } else {
         return luaZ_fill(z);
     }
@@ -52,4 +50,35 @@ fn luaZ_fill(z: *ZIO) callconv(.c) c_int {
     const pout = z.p;
     z.p += 1;
     return pout[0];
+}
+
+pub inline fn luaZ_initbuffer(L: *const clua.lua_State, buff: *Mbuffer) c_int {
+    _ = L;
+    buff.buffer = null;
+    buff.buffsize = 0;
+    return 0;
+}
+
+pub inline fn luaZ_buffer(buff: *const Mbuffer) ?[*:0]const u8 {
+    return buff.buffer;
+}
+
+pub inline fn luaZ_sizebuffer(buff: *const Mbuffer) usize {
+    return buff.buffsize;
+}
+
+pub inline fn luaZ_bufflen(buff: *const Mbuffer) usize {
+    return buff.n;
+}
+
+pub inline fn luaZ_buffremove(buff: *Mbuffer, i: comptime_int) void {
+    buff.n -= i;
+}
+
+pub inline fn luaZ_resizebuffer(L: *clua.lua_State, buff: *Mbuffer, size: comptime_int) void {
+    buff.buffer = cmem.luaM_reallocvchar(L, buff.buffer, buff.buffsize, size);
+}
+
+pub inline fn luaZ_freebuffer(L: *clua.lua_State, buff: *Mbuffer) void {
+    luaZ_resizebuffer(L, buff, 0);
 }
