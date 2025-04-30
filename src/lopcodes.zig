39d6c5cf080f3f418007a7028663d8941496932f
eshom <48328872+eshom@users.noreply.github.com>
2025-04-30 20:25:17 +0300

    lopcodes: Up to the OpCode enum

--- src/lopcodes.zig
+++ src/lopcodes.zig
@@ -205,3 +205,229 @@ pub inline fn GETARG_sJ(i: anytype) comptime_int {
 pub inline fn SETARG_sJ(i: anytype, j: anytype) void {
     setarg(i, j + OFFSET_sJ, POS_sJ, SIZE_sJ);
 }
+
+pub inline fn CREATE_ABCk(
+    o: climits.Instruction,
+    a: climits.Instruction,
+    b: climits.Instruction,
+    c: climits.Instruction,
+    k: climits.Instruction,
+) climits.Instruction {
+    return (o << POS_OP) | (a << POS_A) | (b << POS_B) | (c << POS_C) | (k << POS_k);
+}
+
+pub inline fn CREATE_ABx(
+    o: climits.Instruction,
+    a: climits.Instruction,
+    bc: climits.Instruction,
+) climits.Instruction {
+    return (o << POS_OP) | (a << POS_A) | (bc << POS_Bx);
+}
+
+pub inline fn CREATE_Ax(
+    o: climits.Instruction,
+    a: climits.Instruction,
+) climits.Instruction {
+    return (o << POS_OP) | (a << POS_A);
+}
+
+pub inline fn CREATE_sJ(
+    o: climits.Instruction,
+    j: climits.Instruction,
+    k: climits.Instruction,
+) climits.Instruction {
+    return (o << POS_OP) | (j << POS_sJ) | (k << POS_k);
+}
+
+// TODO: figure this debug business, and does it matter?
+
+// #if !defined(MAXINDEXRK)  /* (for debugging only) */
+// #define MAXINDEXRK MAXARG_B
+// #endif
+
+/// invalid register that fits in 8 bits
+pub const NO_REG = MAXARG_A;
+
+// R[x] - register
+// K[x] - constant (in constant table)
+// RK(x) == if k(i) then K[x] else R[x]
+
+/// Grep "ORDER OP" if you change these enums. Opcodes marked with a (*)
+/// has extra descriptions in the notes after the enumeration.
+pub const OpCode = enum(c_uint) {
+    //----------------------------------------------------------------------
+    //  name         args    description
+    //------------------------------------------------------------------------*/
+    OP_MOVE, //      A B     R[A] := R[B]
+    OP_LOADI, //     A sBx   R[A] := sBx
+    OP_LOADF, //     A sBx   R[A] := (lua_Number)sBx
+    OP_LOADK, //     A Bx    R[A] := K[Bx]
+    OP_LOADKX, //    A       R[A] := K[extra arg]
+    OP_LOADFALSE, // A       R[A] := false
+    OP_LFALSESKIP, //A       R[A] := false; pc++    (*)
+    OP_LOADTRUE, //  A       R[A] := true
+    OP_LOADNIL, //   A B     R[A], R[A+1], ..., R[A+B] := nil
+    OP_GETUPVAL, //  A B     R[A] := UpValue[B]
+    OP_SETUPVAL, //  A B     UpValue[B] := R[A]
+
+    OP_GETTABUP, //  A B C   R[A] := UpValue[B][K[C]:shortstring]
+    OP_GETTABLE, //  A B C   R[A] := R[B][R[C]]
+    OP_GETI, //      A B C   R[A] := R[B][C]
+    OP_GETFIELD, //  A B C   R[A] := R[B][K[C]:shortstring]
+
+    OP_SETTABUP, //  A B C   UpValue[A][K[B]:shortstring] := RK(C)
+    OP_SETTABLE, //  A B C   R[A][R[B]] := RK(C)
+    OP_SETI, //      A B C   R[A][B] := RK(C)
+    OP_SETFIELD, //  A B C   R[A][K[B]:shortstring] := RK(C)
+
+    OP_NEWTABLE, //  A B C k R[A] := {}
+
+    OP_SELF, //      A B C   R[A+1] := R[B]; R[A] := R[B][RK(C):string]
+
+    OP_ADDI, //      A B sC  R[A] := R[B] + sC
+
+    OP_ADDK, //      A B C   R[A] := R[B] + K[C]:number
+    OP_SUBK, //      A B C   R[A] := R[B] - K[C]:number
+    OP_MULK, //      A B C   R[A] := R[B] * K[C]:number
+    OP_MODK, //      A B C   R[A] := R[B] % K[C]:number
+    OP_POWK, //      A B C   R[A] := R[B] ^ K[C]:number
+    OP_DIVK, //      A B C   R[A] := R[B] / K[C]:number
+    OP_IDIVK, //     A B C   R[A] := R[B] // K[C]:number
+
+    OP_BANDK, //     A B C   R[A] := R[B] & K[C]:integer
+    OP_BORK, //      A B C   R[A] := R[B] | K[C]:integer
+    OP_BXORK, //     A B C   R[A] := R[B] ~ K[C]:integer
+
+    OP_SHRI, //      A B sC  R[A] := R[B] >> sC
+    OP_SHLI, //      A B sC  R[A] := sC << R[B]
+
+    OP_ADD, //       A B C   R[A] := R[B] + R[C]
+    OP_SUB, //       A B C   R[A] := R[B] - R[C]
+    OP_MUL, //       A B C   R[A] := R[B] * R[C]
+    OP_MOD, //       A B C   R[A] := R[B] % R[C]
+    OP_POW, //       A B C   R[A] := R[B] ^ R[C]
+    OP_DIV, //       A B C   R[A] := R[B] / R[C]
+    OP_IDIV, //      A B C   R[A] := R[B] // R[C]
+
+    OP_BAND, //      A B C   R[A] := R[B] & R[C]
+    OP_BOR, //       A B C   R[A] := R[B] | R[C]
+    OP_BXOR, //      A B C   R[A] := R[B] ~ R[C]
+    OP_SHL, //       A B C   R[A] := R[B] << R[C]
+    OP_SHR, //       A B C   R[A] := R[B] >> R[C]
+
+    OP_MMBIN, //     A B C    call C metamethod over R[A] and R[B]    (*)
+    OP_MMBINI, //    A sB C k call C metamethod over R[A] and sB
+    OP_MMBINK, //    A B C k  call C metamethod over R[A] and K[B]
+
+    OP_UNM, //       A B     R[A] := -R[B]
+    OP_BNOT, //      A B     R[A] := ~R[B]
+    OP_NOT, //       A B     R[A] := not R[B]
+    OP_LEN, //       A B     R[A] := #R[B] (length operator)
+
+    OP_CONCAT, //    A B     R[A] := R[A].. ... ..R[A + B - 1]
+
+    OP_CLOSE, //     A       close all upvalues >= R[A]
+    OP_TBC, //       A       mark variable A "to be closed"
+    OP_JMP, //       sJ      pc += sJ
+    OP_EQ, //        A B k   if ((R[A] == R[B]) ~= k) then pc++
+    OP_LT, //        A B k   if ((R[A] <  R[B]) ~= k) then pc++
+    OP_LE, //        A B k   if ((R[A] <= R[B]) ~= k) then pc++
+
+    OP_EQK, //       A B k   if ((R[A] == K[B]) ~= k) then pc++
+    OP_EQI, //       A sB k  if ((R[A] == sB) ~= k) then pc++
+    OP_LTI, //       A sB k  if ((R[A] < sB) ~= k) then pc++
+    OP_LEI, //       A sB k  if ((R[A] <= sB) ~= k) then pc++
+    OP_GTI, //       A sB k  if ((R[A] > sB) ~= k) then pc++
+    OP_GEI, //       A sB k  if ((R[A] >= sB) ~= k) then pc++
+
+    OP_TEST, //      A k     if (not R[A] == k) then pc++
+    OP_TESTSET, //   A B k   if (not R[B] == k) then pc++ else R[A] := R[B] (*)
+
+    OP_CALL, //      A B C   R[A], ... ,R[A+C-2] := R[A](R[A+1], ... ,R[A+B-1])
+    OP_TAILCALL, //  A B C k return R[A](R[A+1], ... ,R[A+B-1])
+
+    OP_RETURN, //    A B C k return R[A], ... ,R[A+B-2]    (see note)
+    OP_RETURN0, //           return
+    OP_RETURN1, //   A       return R[A]
+
+    OP_FORLOOP, //   A Bx    update counters; if loop continues then pc-=Bx;
+    OP_FORPREP, //   A Bx    <check values and prepare counters>;
+    //                       if not to run then pc+=Bx+1;
+
+    OP_TFORPREP, //  A Bx    create upvalue for R[A + 3]; pc+=Bx
+    OP_TFORCALL, //  A C     R[A+4], ... ,R[A+3+C] := R[A](R[A+1], R[A+2]);
+    OP_TFORLOOP, //  A Bx    if R[A+2] ~= nil then { R[A]=R[A+2]; pc -= Bx }
+
+    OP_SETLIST, //   A B C k R[A][C+i] := R[A+i], 1 <= i <= B
+
+    OP_CLOSURE, //   A Bx    R[A] := closure(KPROTO[Bx])
+
+    OP_VARARG, //    A C     R[A], R[A+1], ..., R[A+C-2] = vararg
+
+    OP_VARARGPREP, //A       (adjust vararg parameters)
+
+    OP_EXTRAARG, //  Ax      extra (larger) argument for previous opcode
+};
+
+pub const NUM_OPCODES = @intFromEnum(OpCode.OP_EXTRAARG) + 1;
+
+//===========================================================================
+//  Notes:
+//
+//  (*) Opcode OP_LFALSESKIP is used to convert a condition to a boolean
+//  value, in a code equivalent to (not cond ? false : true).  (It
+//  produces false and skips the next instruction producing true.)
+//
+//  (*) Opcodes OP_MMBIN and variants follow each arithmetic and
+//  bitwise opcode. If the operation succeeds, it skips this next
+//  opcode. Otherwise, this opcode calls the corresponding metamethod.
+//
+//  (*) Opcode OP_TESTSET is used in short-circuit expressions that need
+//  both to jump and to produce a value, such as (a = b or c).
+//
+//  (*) In OP_CALL, if (B == 0) then B = top - A. If (C == 0), then
+//  'top' is set to last_result+1, so next open instruction (OP_CALL,
+//  OP_RETURN*, OP_SETLIST) may use 'top'.
+//
+//  (*) In OP_VARARG, if (C == 0) then use actual number of varargs and
+//  set top (like in OP_CALL with C == 0).
+//
+//  (*) In OP_RETURN, if (B == 0) then return up to 'top'.
+//
+//  (*) In OP_LOADKX and OP_NEWTABLE, the next instruction is always
+//  OP_EXTRAARG.
+//
+//  (*) In OP_SETLIST, if (B == 0) then real B = 'top'; if k, then
+//  real C = EXTRAARG _ C (the bits of EXTRAARG concatenated with the
+//  bits of C).
+//
+//  (*) In OP_NEWTABLE, B is log2 of the hash size (which is always a
+//  power of 2) plus 1, or zero for size zero. If not k, the array size
+//  is C. Otherwise, the array size is EXTRAARG _ C.
+//
+//  (*) For comparisons, k specifies what condition the test should accept
+//  (true or false).
+//
+//  (*) In OP_MMBINI/OP_MMBINK, k means the arguments were flipped
+//   (the constant is the first operand).
+//
+//  (*) All 'skips' (pc++) assume that next instruction is a jump.
+//
+//  (*) In instructions OP_RETURN/OP_TAILCALL, 'k' specifies that the
+//  function builds upvalues, which may need to be closed. C > 0 means
+//  the function is vararg, so that its 'func' must be corrected before
+//  returning; in this case, (C - 1) is its number of fixed parameters.
+//
+//  (*) In comparisons with an immediate operand, C signals whether the
+//  original operand was a float. (It must be corrected in case of
+//  metamethods.)
+//
+//===========================================================================*/
+
+// masks for instruction properties. The format is:
+// bits 0-2: op mode
+// bit 3: instruction set register A
+// bit 4: operator is a test (next instruction must be a jump)
+// bit 5: instruction uses 'L->top' set by previous instruction (when B == 0)
+// bit 6: instruction sets 'L->top' for next instruction (when C == 0)
+// bit 7: instruction is an MM instruction (call a metamethod)
